81d43c5c419f7c441e5998342e7b4133c0cf8807
luciano-mota <amoosenhorjesus3@gmail.com>
2025-11-29 17:10:21 -0300

    ajuste no mapper

--- book-management/src/main/java/com/book/management/application/mapper/BookMapper.java
+++ book-management/src/main/java/com/book/management/application/mapper/BookMapper.java
@@ -1,10 +1,10 @@
 package com.book.management.application.mapper;
 
-import com.book.management.application.dto.request.BookRequestDTO;
-import com.book.management.application.dto.response.BookResponseDTO;
+import com.book.management.application.controller.response.BookResponseDTO;
 import com.book.management.domain.model.Book;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+import org.openapitools.model.BookRequestDTO;
 
 @Mapper
 public interface BookMapper {

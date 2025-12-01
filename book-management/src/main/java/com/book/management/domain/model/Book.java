6331feb44568b1668b0d3937a936b7cbaa99a6e7
luciano-mota <amoosenhorjesus3@gmail.com>
2025-11-30 22:21:32 -0300

    Ajustes

--- book-management/src/main/java/com/book/management/domain/model/Book.java
+++ book-management/src/main/java/com/book/management/domain/model/Book.java
@@ -1,33 +1,38 @@
 package com.book.management.domain.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.List;
 
 public class Book {
 
   private Long id;
+  private Integer bookCode;
   private String title;
-  private LocalDate publicationDate;
-  private BigDecimal value;
+  private String publisher;
+  private String edition;
+  private String publicationDate;
+  private BigDecimal price;
   private List<Long> authorsIds;
   private List<Long> subjectsIds;
 
   public Book() {}
 
-  public Book(Long id, String title, LocalDate publicationDate, BigDecimal value,
+  public Book(Long id, Integer bookCode, String title, String publisher, String edition, String publicationDate, BigDecimal price,
       List<Long> authorsIds, List<Long> subjectsIds) {
     this.id = id;
+    this.bookCode = bookCode;
     this.title = title;
+    this.publisher = publisher;
+    this.edition = edition;
     this.publicationDate = publicationDate;
-    this.value = value;
+    this.price = price;
     this.authorsIds = authorsIds;
     this.subjectsIds = subjectsIds;
   }
 
-  public static Book newBook(String title, LocalDate publicationDate, BigDecimal value,
+  public static Book newBook(Integer bookCode, String title, String publisher, String edition, String publicationDate, BigDecimal price,
       List<Long> authorsIds, List<Long> subjectsIds) {
-    return new Book(null, title, publicationDate, value, authorsIds, subjectsIds);
+    return new Book(null, bookCode, title, publisher, edition, publicationDate, price, authorsIds, subjectsIds);
   }
 
   public Long getId() {
@@ -38,6 +43,14 @@ public class Book {
     this.id = id;
   }
 
+  public Integer getBookCode() {
+    return bookCode;
+  }
+
+  public void setBookCode(Integer bookCode) {
+    this.bookCode = bookCode;
+  }
+
   public String getTitle() {
     return title;
   }
@@ -46,20 +59,36 @@ public class Book {
     this.title = title;
   }
 
-  public LocalDate getPublicationDate() {
+  public String getPublisher() {
+    return publisher;
+  }
+
+  public void setPublisher(String publisher) {
+    this.publisher = publisher;
+  }
+
+  public String getEdition() {
+    return edition;
+  }
+
+  public void setEdition(String edition) {
+    this.edition = edition;
+  }
+
+  public String getPublicationDate() {
     return publicationDate;
   }
 
-  public void setPublicationDate(LocalDate publicationDate) {
+  public void setPublicationDate(String publicationDate) {
     this.publicationDate = publicationDate;
   }
 
-  public BigDecimal getValue() {
-    return value;
+  public BigDecimal getPrice() {
+    return price;
   }
 
-  public void setValue(BigDecimal value) {
-    this.value = value;
+  public void setPrice(BigDecimal price) {
+    this.price = price;
   }
 
   public List<Long> getAuthorsIds() {

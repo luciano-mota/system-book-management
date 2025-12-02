f8afeb2b49438ab82cb72ad4c49a26c91306a0c0
luciano-mota <amoosenhorjesus3@gmail.com>
2025-12-01 22:26:28 -0300

    Add tratamento de excecao, add annotation explicita de transação e implementado metodo update

--- book-management/src/main/java/com/book/management/infrastructure/persistence/impl/AuthorRepositoryImpl.java
+++ book-management/src/main/java/com/book/management/infrastructure/persistence/impl/AuthorRepositoryImpl.java
@@ -2,13 +2,16 @@ package com.book.management.infrastructure.persistence.impl;
 
 import com.book.management.domain.model.Author;
 import com.book.management.domain.repository.AuthorRepository;
+import com.book.management.infrastructure.exception.IsDataBaseException;
 import com.book.management.infrastructure.exception.IsNotFoundException;
 import com.book.management.infrastructure.persistence.entity.AuthorEntity;
 import com.book.management.infrastructure.persistence.repository.AuthorJpaRepository;
-import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @RequiredArgsConstructor
@@ -24,7 +27,7 @@ public class AuthorRepositoryImpl implements AuthorRepository {
   }
 
   @Override
-  @Transactional
+  @Transactional(readOnly = true)
   public Author findById(Long id) {
     var authorEntity = authorJpaRepository.findById(id)
         .orElseThrow(() -> new IsNotFoundException("Author not found with id: " + id));
@@ -32,6 +35,7 @@ public class AuthorRepositoryImpl implements AuthorRepository {
   }
 
   @Override
+  @Transactional(readOnly = true)
   public List<Author> findAll() {
     return authorJpaRepository.findAll().stream()
         .map(authorEntity -> new Author(authorEntity.getId(), authorEntity.getName()))
@@ -39,7 +43,24 @@ public class AuthorRepositoryImpl implements AuthorRepository {
   }
 
   @Override
+  @Transactional
+  public Author update(Author author) {
+    var authorEntity = authorJpaRepository.findById(author.getId())
+        .orElseThrow(() -> new IsNotFoundException("Author not found with id: " + author.getId()));
+    authorEntity.setName(author.getName());
+    authorJpaRepository.save(authorEntity);
+    return new Author(authorEntity.getId(), authorEntity.getName());
+  }
+
+  @Override
+  @Transactional
   public void deleteById(Long id) {
-    authorJpaRepository.deleteById(id);
+    try {
+      authorJpaRepository.deleteById(id);
+    } catch (EmptyResultDataAccessException e) {
+      throw new IsNotFoundException("Author not found with id: " + id);
+    } catch (DataIntegrityViolationException e) {
+      throw new IsDataBaseException("Integrity violation");
+    }
   }
 }

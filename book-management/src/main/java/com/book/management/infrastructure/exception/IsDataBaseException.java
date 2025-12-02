317d04496057adeb97bafa5fd6ce815ebc6746fe
luciano-mota <amoosenhorjesus3@gmail.com>
2025-12-01 22:28:42 -0300

    Criado Tratamento de excecao personalizado

---
+++ book-management/src/main/java/com/book/management/infrastructure/exception/IsDataBaseException.java
@@ -0,0 +1,8 @@
+package com.book.management.infrastructure.exception;
+
+public class IsDataBaseException extends RuntimeException{
+
+  public IsDataBaseException(String message) {
+    super(message);
+  }
+}
